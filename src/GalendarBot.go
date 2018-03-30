f53cddbdb37ae22a2ca66d239374b7468c3198ae
Ilya <il07ya96@mail.ru>
2018-03-30 20:28:50 +0800

    restructing

--- src/GalendarBot.go
+++ src/GalendarBot.go
@@ -1,4 +1,4 @@
-package GalendarBot
+package main
 
 import (
 	"log"
--- src/handler/bot_Telegram.go
+++ src/handler/bot_Telegram.go
@@ -1,31 +1,35 @@
-package main
+package handler
 
 //go run bot_Telegram.go GalendarBot.go
 import (
 	"fmt"
 	"log"
 
-	"./GalendarBot"
 	"github.com/Syfaro/telegram-bot-api"
 )
 
-func main() {
+func handler(chan) {
 	bot, err := tgbotapi.NewBotAPI("559800435:AAE_aExKTPXbcwEto2qsHTHux_Wlh5McQic")
 	if err != nil {
 		// log.Panic(err)
 		log.Println(err)
 	}
+	
 	// bot.Debug = true
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 	// инициализируем канал, куда будут прилетать обновления от API
+	
 	var ucfg tgbotapi.UpdateConfig = tgbotapi.NewUpdate(0)
 	ucfg.Timeout = 60
 	// читаем обновления из канала
+	
 	updates, err := bot.GetUpdatesChan(ucfg)
+	
 	if err != nil {
 		// log.Panic(err)
 		log.Println(err)
 	}
+	
 	for update := range updates {
 		// Пользователь, который написал боту
 		UserName := update.Message.From.UserName
@@ -38,11 +42,11 @@ func main() {
 		var reply string
 		var Text string
 		if update.Message.Command() != "" {
-			reply = GalendarBot.ParseCommand(update.Message.Command(), fmt.Sprint(userID))
+			//reply = GalendarBot.ParseCommand(update.Message.Command(), fmt.Sprint(userID))
 		} else {
 			//Получем сообщение и парсим его
 			Text = update.Message.Text
-			reply = GalendarBot.ParseText(Text, fmt.Sprint(userID))
+			//reply = GalendarBot.ParseText(Text, fmt.Sprint(userID))
 		}
 
 		log.Printf("[%s] %d %s", UserName, ChatID, Text)
